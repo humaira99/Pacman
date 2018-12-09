e08dce6a19c67aac7f737a5c4b382bf9020ba688
psyha5 <psyha5@exmail.nottingham.ac.uk>
2018-12-09 06:43:07 +0000

    javadocs score

--- src/gameboard/Score.java
+++ src/gameboard/Score.java
@@ -7,8 +7,15 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
+/**
+ * Displays the players score and number of lives
+ */
 public class Score {
 
+    /**
+     * gets the score
+     * @return current score
+     */
     public Text getScore() {
         return score;
     }
@@ -16,6 +23,11 @@ public class Score {
     public Text score;
     public Text lifes;
 
+    /**
+     * Displays the players score
+     * Displays the number of lives
+     * @param root Group where the score and lives labels are added
+     */
     Score(Group root) {
         this.score = new Text(BarObstacle.THICKNESS * 4, BarObstacle.THICKNESS * 28, "Score: 0");
         this.lifes = new Text(BarObstacle.THICKNESS * 20, BarObstacle.THICKNESS * 28,"Lifes: 3");
