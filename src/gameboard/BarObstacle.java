cf2377f61c520cd0fa19af428642fbae2f5467a1
psyha5 <psyha5@exmail.nottingham.ac.uk>
2018-12-09 06:35:27 +0000

    javadocs barobstacle

--- src/gameboard/BarObstacle.java
+++ src/gameboard/BarObstacle.java
@@ -4,13 +4,16 @@ package gameboard;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
+/**
+ * Creates a bar obstacle for the maze i.e. a wall/island
+ */
 public class BarObstacle extends Rectangle {
 
     public static double THICKNESS = 25;
     /**
-     *
-     * @param x
-     * @param y
+     * Makes a new obstacle with the set orientation and length
+     * @param x x-coordinate of the bar
+     * @param y y-coordinate of the bar
      * @param orientation - horizontal or vertical
      * @param length - the length of the bar (1 == 100px)
      */
